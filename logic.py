e81860fcb4d1d97868a90fec823f56e8066564c3
Ronald Grey <loganbennett102@gmail.com>
2025-11-26 20:01:22 -0600

    Changed the way categories work. There is now a default selection of categories, with the ability to simply add more. This was changed because allowing people to type whatever for category would probably mess up sorting / displaying data.
    + few other random changes

--- logic.py
+++ logic.py
@@ -14,21 +14,80 @@ transactions = [
     # }
 ]
 
-def load_data():
+income_categories = []
+expense_categories = []
+
+
+def load_transaction_data():
     global transactions
-    with open("data.json", "r") as file:
+    with open("transaction_data.json", "r") as file:
         transactions = json.load(file)
 
 
-def save_data():
-    with open("data.json", "w") as file:
+def save_transaction_data():
+    with open("transaction_data.json", "w") as file:
         json.dump(transactions, file, indent=4)
+
+
+def load_categories():
+    global income_categories
+    global expense_categories
+    with open("categories.json", "r") as file:
+        data = json.load(file)
+
+    income_categories = data["income"]
+    expense_categories = data["expense"]
+
+def save_categories():
+    with open("categories.json", "w") as file:
+        data = {
+            "income": income_categories,
+            "expense": expense_categories
+        }
+        json.dump(data, file, indent=4)
+
 
 def main_menu():
     selection = input("what would you like to do? \n 1. Add transaction \n 2. Review transactions \n 3. Delete transaction \n 4. quit (please respond with 1, 2, 3, or 4)")
     if selection not in {"1", "2", "3", "4"}:
         print("invalid input. Please try again.")
     return selection
+
+# Make two if statements that legit just link the correct list instead of having two if statements with damn near same logic to handle expense and income categories. Also use len() +1 instead of index for the last option #
+def category_selection(transaction_type, category):
+    global expense_categories
+    global income_categories
+    print(transaction_type)
+    if transaction_type == "Expense":
+        category = expense_categories
+    elif transaction_type == "Income":
+        category = income_categories
+    else:
+        print("an error has occurred please try again.")
+        run()
+
+    print(category)
+    for index, cat_name in enumerate(category, start = 1):
+        print(f"{index}. {cat_name}")
+    print(len(category))
+    last_option = len(category)
+    last_option += 1
+
+    print(f"{last_option}.create new category")
+    category_input_selection = input("please enter the number value of category you want to use for this transaction, or create a new one(last option in selection of categories).")
+    category_input = int(category_input_selection)
+    print(category_input)
+    print(last_option)
+    if category_input == last_option:
+         new_category = input("please input new category name")
+         category.append(new_category)
+         category = new_category
+         return category
+    else:
+        category = category[category_input - 1]
+        return category
+
+
 
 
 def add_transaction():
@@ -38,7 +97,10 @@ def add_transaction():
     elif transaction_type == "2":
         transaction_type = "Income"
 
-    category = input("please input the category (i.e. food, gas, electric bill")
+    #category = input("please input the category (i.e. food, gas, electric bill")
+    category = ''
+
+    category = category_selection(transaction_type, category)
 
     amount_string = input("please input the amount of transaction")
     amount = float(amount_string)
@@ -87,23 +149,31 @@ def delete_transaction():
 
 
 def run():
-   load_data()
+   load_transaction_data()
+   load_categories()
    while True:
     selection = main_menu()
 
     if selection == "1":
         add_transaction()
-        save_data()
+        save_transaction_data()
+        save_categories()
     if selection == "2":
         review_transaction()
 
     if selection == "3":
        delete_transaction()
-       save_data()
+       save_transaction_data()
     if selection == "4":
+        save_transaction_data()
+        save_categories()
         break
 
-
+print(expense_categories)
+print(income_categories)
 run()
+
+
+
 
 
